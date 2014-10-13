1bce1ad477d165205457bed75fbaed2cb4326bec
Ubuntu <gerbl.alexander@gmail.com>
2014-10-13 12:34:36 +0200

    AddChild added & tested!

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 typedef uint8_t Key [8];
 typedef int Value;
 
-const uint8_t EMPTY = -1;
+const uint8_t EMPTY(-1);
 
 enum class Nodetype : uint8_t { SVLeaf = 1, Node4 = 2, Node16 = 3, Node48 = 4, Node256 = 5 };
 
@@ -79,7 +79,7 @@ struct Node256 : Node
  * @brief: returns whether a node is a leaf or not
  * @params:	node 		node to check for being a leaf
  **/
-bool isLeaf(shared_ptr<BaseNode> node)
+bool inline isLeaf(shared_ptr<BaseNode> node)
 {
 	return (node->type == Nodetype::SVLeaf);
 }
@@ -153,12 +153,56 @@ shared_ptr<BaseNode> search(shared_ptr<BaseNode> node, const Key& key, uint8_t d
 	depth += node->prefixLen;
 	auto next = findChild(node, key[depth]);
 	return search(next, key, depth+1);
+} 
+/// Tested - take care as script tells parent musn't be a SVLeaf and parent musn't be full
+/// Appends a new child to an inner node NUMCHILDREN++
+void addChild(shared_ptr<BaseNode>& parent, uint8_t byte, shared_ptr<BaseNode>& child)
+{
+	//Case for total beginning root of ART = nullptr! ! !
+	if(parent == nullptr)
+		parent = child;
+	if(parent->type == Nodetype::Node4)
+	{
+		auto tmp_parent = static_pointer_cast<Node4>(parent);
+		tmp_parent->keys[tmp_parent->numChildren] = byte;
+		tmp_parent->numChildren++;
+		sort(tmp_parent->keys.begin(), tmp_parent->keys.begin() + (tmp_parent->numChildren));
+		auto index_itr = find(tmp_parent->keys.begin(), tmp_parent->keys.end(), byte);
+		auto index = index_itr - tmp_parent->keys.begin();
+		copy(tmp_parent->child.begin() + index, tmp_parent->child.begin() + tmp_parent->numChildren, tmp_parent->child.begin() + index + 1);
+		tmp_parent->child[index] = child;
+
+		return;
+	}
+	if(parent->type == Nodetype::Node16)
+	{
+		auto tmp_parent = static_pointer_cast<Node16>(parent);
+		tmp_parent->keys[tmp_parent->numChildren] = byte;
+		tmp_parent->numChildren++;
+		sort(tmp_parent->keys.begin(), tmp_parent->keys.begin() + (tmp_parent->numChildren));
+		auto index_itr = find(tmp_parent->keys.begin(), tmp_parent->keys.end(), byte);
+		auto index = index_itr - tmp_parent->keys.begin();
+		copy(tmp_parent->child.begin() + index, tmp_parent->child.begin() + tmp_parent->numChildren, tmp_parent->child.begin() + index + 1);
+		tmp_parent->child[index] = child;
+		return;
+	}
+	if(parent->type == Nodetype::Node48)
+	{
+		auto tmp_parent = static_pointer_cast<Node48>(parent);
+		tmp_parent->child[tmp_parent->numChildren] = child;
+		tmp_parent->index[byte] = tmp_parent->numChildren;		
+		tmp_parent->numChildren++;	
+		return;	
+	}
+	auto tmp_parent = static_pointer_cast<Node256>(parent);
+	tmp_parent->child[byte] = child;
+	tmp_parent->numChildren++;
 }
 
 int main()
 {
 //Build nodes
-	shared_ptr<Node4> n0 = make_shared<Node4>();
+/*	shared_ptr<Node4> n0 = make_shared<Node4>();
 	shared_ptr<Node16> n1 = make_shared<Node16>();
 	shared_ptr<Node48> n2 = make_shared<Node48>();
 	shared_ptr<Node256> n3 = make_shared<Node256>();
@@ -201,13 +245,13 @@ int main()
 	n0->keys[1] = 0x08;
 	n0->child[0] = leaf;
 	n0->child[1] = leaf2;
-/*	n1->keys[0] = 0x01;
+	n1->keys[0] = 0x01;
 	n2->index[0x02] = 0;
 	n4->keys[0] = 0x04;
 	n5->keys[0] = 0x05;
 	n6->keys[0] = 0x06;
 	n7->keys[0] = 0x07;
-*/	
+/	
 // 
 //	n0->child[0] = n1;
 //	n1->child[0] = n2;
@@ -219,9 +263,16 @@ int main()
 	n7->child[0] = leaf;
 
 	Key test_key = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
-
-	cout << "Should be = " << leaf << " is "<< findChild(n0, test_key[7]) << endl;
-	cout << "search : " << search(n0, test_key, 0) << " should be = "<< leaf << endl;
+*/
+	shared_ptr<BaseNode> root =  make_shared<Node256>(); //why no auto ?
+	shared_ptr<BaseNode> leaf = make_shared<SVLeaf>();
+	cout << "Leaf = " << leaf << endl;
+	shared_ptr<BaseNode> leaf2 = make_shared<SVLeaf>();
+	cout << "Leaf2 = " << leaf2 << endl;
+	addChild(root, 0x01, leaf);
+	addChild(root, 0x00, leaf2);
+	cout << "Found leaf at = " << findChild(root, 0x01) << endl;
+	cout << "Found leaf2 at = " << findChild(root, 0x00) << endl;
 
 	return 0;
 }
